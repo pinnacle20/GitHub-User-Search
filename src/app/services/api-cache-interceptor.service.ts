1cc8ea28857df922c36b6fe6d260f5a505de609d
shivanand <shivanandchoudhary22@gmail.com>
2024-02-08 02:43:38 +0530

    added comment

--- src/app/services/api-cache-interceptor.service.ts
+++ src/app/services/api-cache-interceptor.service.ts
@@ -18,11 +18,16 @@ export class ApiCacheInterceptorService implements HttpInterceptor{
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
+    //checks if the request endpoint is present in the set of endpoints to cache    
     if(this.endpointsToCache.has(req.url)){
       const cachedResponse = this.cache.get(req.url);
+      
+      //if the response is found in the cache, returns it as an observable of HttpResponse
       if(cachedResponse){
         return of(cachedResponse);
       }
+
+      //if the response is not found, send the requestand and cache the response
       return next.handle(req).pipe(
         tap((response) => {
           if (response instanceof HttpResponse) {
@@ -32,6 +37,8 @@ export class ApiCacheInterceptorService implements HttpInterceptor{
       );
       
     }
+
+    //if the request enpoint isn't in the set of endpoint, send the request as it is to the server
     return next.handle(req);
   }
 
