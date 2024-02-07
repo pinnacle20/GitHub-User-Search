8f8cabf6af100437bbab9d7f8576a82db3ec032a
shivanand <shivanandchoudhary22@gmail.com>
2024-02-08 01:37:38 +0530

    creating getRepos() service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+    return this.httpClient.get(
+      `https://api.github.com/users/${githubUsername}`
+    );
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  // implement getRepos method by referring to the documentation. Add proper types for the return type and params
+  getRepos(githubUsername: string, pageNo: number, perPage: number) {
+    const BASEURL = `https://api.github.com/users/${githubUsername}/repos`;
+
+    // Setting up query parameters for pagination
+    const params = {
+      page: pageNo.toString(),
+      per_page: perPage.toString(),
+    };
+    // Making the HTTP GET request with pagination parameters
+    return this.httpClient.get<any[]>(BASEURL, { params });
+  }
 }
