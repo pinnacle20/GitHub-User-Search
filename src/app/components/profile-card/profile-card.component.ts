41f2d014fb3e8a5893e11b57ecba28758a38680e
shivanand <shivanandchoudhary22@gmail.com>
2024-02-08 21:28:51 +0530

    profile card

--- src/app/components/profile-card/profile-card.component.ts
+++ src/app/components/profile-card/profile-card.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.scss']
+  styleUrls: ['./profile-card.component.scss'],
 })
 export class ProfileCardComponent {
+  constructor(private apiService: ApiService) {}
 
+  respo: any[] = [];
+
+  profileImg = '';
+  userName = '';
+  userBio = '';
+  userLocation = '';
+  gitHubHandle = '';
+  twitterHandle = '';
+  emailId = '';
+  followers = '';
+  following = '';
+
+  ngOnInit() {
+    this.apiService.getUser('Swarnendu0123').subscribe((data) => {
+      this.userName = data.name;
+      this.userBio = data.bio === null ? 'NA' : data.bio;
+      this.userLocation = data.location === null ? 'NA' : data.location;
+      this.twitterHandle =
+        data.twitter_username === null ? 'NA' : data.twitter_username;
+      this.emailId = data.email === null ? 'NA' : data.email;
+      this.gitHubHandle = data.html_url;
+      this.followers = data.followers;
+      this.following = data.following;
+      this.profileImg = data.avatar_url;
+    });
+  }
 }
--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -8,33 +8,4 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent {
-  constructor(private apiService: ApiService) {}
-  
-  respo: any[] = [];
-
-  profileImg  = '';
-  userName = '';
-  userBio = '';
-  userLocation = '';
-  gitHubHandle = '';
-  twitterHandle = '';
-  emailId = '';
-  followers = '';
-  following = '';
-
-  ngOnInit() {
-    this.apiService.getUser('Swarnendu0123').subscribe((data)=>{
-      console.log(data)
-      this.userName = data.name;
-      this.userBio = (data.bio===null)?'NA':data.bio;
-      this.userLocation =(data.location===null)?'NA':data.location;
-      this.twitterHandle = (data.twitter_username===null)?'NA':data.twitter_username ;
-      this.emailId = (data.email===null)?'NA':data.email ;
-      this.gitHubHandle = data.html_url;
-      this.followers = data.followers;
-      this.following = data.following;
-      this.profileImg = data.avatar_url;
-    });
-  }
-
 }
