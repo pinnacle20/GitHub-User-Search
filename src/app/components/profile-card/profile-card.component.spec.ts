a15db36b2528fc0caade4c48e6539e122def710b
shivanand <shivanandchoudhary22@gmail.com>
2024-02-12 18:01:20 +0530

    profile card component test cases

--- src/app/components/profile-card/profile-card.component.spec.ts
+++ src/app/components/profile-card/profile-card.component.spec.ts
@@ -1,21 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
 import { ProfileCardComponent } from './profile-card.component';
+import { ApiService } from 'src/app/services/api.service';
+import { SimpleChanges } from '@angular/core';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProfileCardComponent', () => {
   let component: ProfileCardComponent;
   let fixture: ComponentFixture<ProfileCardComponent>;
+  let apiService: ApiService;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProfileCardComponent],
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [ApiService],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProfileCardComponent]
-    });
     fixture = TestBed.createComponent(ProfileCardComponent);
     component = fixture.componentInstance;
+    apiService = TestBed.inject(ApiService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
 });
