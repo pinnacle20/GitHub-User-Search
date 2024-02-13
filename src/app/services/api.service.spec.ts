bdf127bd33f767eda4052593a352d84ca242d191
shivanand <shivanandchoudhary22@gmail.com>
2024-02-13 15:06:58 +0530

    api service test cases

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,9 +1,12 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
-  let service: ApiService;
+  let apiService: ApiService;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
@@ -11,7 +14,7 @@ describe('ApiService', () => {
       imports: [HttpClientTestingModule],
       providers: [ApiService],
     });
-    service = TestBed.inject(ApiService);
+    apiService = TestBed.inject(ApiService);
     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
@@ -20,7 +23,80 @@ describe('ApiService', () => {
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(apiService).toBeTruthy();
   });
 
+  it('should get user details', () => {
+    const mockUserData = { login: 'testUser', name: 'Test User' };
+    const githubUsername = 'testUser';
+
+    apiService.getUser(githubUsername).subscribe((userData) => {
+      expect(userData).toEqual(mockUserData);
+    });
+
+    const req = httpTestingController.expectOne(
+      `https://api.github.com/users/${githubUsername}`
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockUserData);
+  });
+
+  it('should handle user details error', () => {
+    const githubUsername = 'nonExistentUser';
+
+    apiService.getUser(githubUsername).subscribe(
+      () => fail('should not reach here'),
+      (error) => {
+        expect(error).toBeTruthy();
+      }
+    );
+
+    const req = httpTestingController.expectOne(
+      `https://api.github.com/users/${githubUsername}`
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.error(new ErrorEvent('HttpErrorResponse'));
+  });
+
+  it('should get user repositories', () => {
+    const mockReposData = [{ name: 'repo1' }, { name: 'repo2' }];
+    const githubUsername = 'testUser';
+    const pageNo = 1;
+    const perPage = 10;
+
+    apiService
+      .getRepos(githubUsername, pageNo, perPage)
+      .subscribe((reposData) => {
+        expect(reposData).toEqual(mockReposData);
+      });
+
+    const req = httpTestingController.expectOne(
+      `https://api.github.com/users/${githubUsername}/repos?page=${pageNo}&per_page=${perPage}`
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockReposData);
+  });
+
+  it('should handle user repositories error', () => {
+    const githubUsername = 'nonExistentUser';
+    const pageNo = 1;
+    const perPage = 10;
+
+    apiService.getRepos(githubUsername, pageNo, perPage).subscribe(
+      () => fail('should not reach here'),
+      (error) => {
+        expect(error).toBeTruthy();
+      }
+    );
+
+    const req = httpTestingController.expectOne(
+      `https://api.github.com/users/${githubUsername}/repos?page=${pageNo}&per_page=${perPage}`
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.error(new ErrorEvent('HttpErrorResponse'));
+  });
 });
